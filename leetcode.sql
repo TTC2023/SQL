991a83de3ff3770c7059ad2c13745e2efd30106a
Tyler Correll <corrett10@gmail.com>
2023-12-05 11:34:54 -0600

    query

--- leetcode.sql
+++ leetcode.sql
@@ -1,2 +1,6 @@
 SELECT PERSON.firstName, PERSON.lastName, ADDRESS.city, ADDRESS.state FROM ADDRESS
 RIGHT JOIN PERSON ON ADDRESS.personId = PERSON.personId;
+
+SELECT tweet_id
+FROM Tweets
+WHERE length(content) > 15;
