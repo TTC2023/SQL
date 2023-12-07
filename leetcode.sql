bc1c8ef330e6c156944fa474dc4d2c5e5e9c92ff
Tyler Correll <corrett10@gmail.com>
2023-12-06 20:57:11 -0600

    sql

--- leetcode.sql
+++ leetcode.sql
@@ -4,3 +4,8 @@ RIGHT JOIN PERSON ON ADDRESS.personId = PERSON.personId;
 SELECT tweet_id
 FROM Tweets
 WHERE length(content) > 15;
+
+SELECT employee.name, bonus.bonus
+From Employee
+Left Join Bonus On Employee.empId = Bonus.empId
+Where bonus.bonus < 1000 or bonus.bonus is Null
