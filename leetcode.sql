7bcce7673b341f50cc4940fa749ac2d7f77e2e38
Tyler Correll <corrett10@gmail.com>
2023-12-07 10:43:46 -0600

    sql

--- leetcode.sql
+++ leetcode.sql
@@ -9,3 +9,13 @@ SELECT employee.name, bonus.bonus
 From Employee
 Left Join Bonus On Employee.empId = Bonus.empId
 Where bonus.bonus < 1000 or bonus.bonus is Null
+
+SELECT
+    contest_id,
+    ROUND(
+        (COUNT(DISTINCT user_id) / (SELECT COUNT(user_id) FROM users)) * 100,
+        2
+    ) AS percentage
+FROM Register
+GROUP BY contest_id
+ORDER BY percentage DESC;
