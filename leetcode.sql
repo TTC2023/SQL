39a530a33d2afd05838c6f56821e5d1d04bddff5
Tyler Correll <corrett10@gmail.com>
2024-04-09 10:56:16 -0500

    leetcode

--- leetcode.sql
+++ leetcode.sql
@@ -409,3 +409,12 @@ FROM Users u
 LEFT JOIN Rides r ON u.id = r.user_id
 GROUP BY u.id
 ORDER By travelled_distance DESC, name ASC
+
+-- cross join is very handy when calculating for each row of a certain table
+
+SELECT s.student_id, s.student_name, sub.subject_name, COUNT(e.subject_name) AS attended_exams
+FROM Students s
+CROSS JOIN Subjects sub
+LEFT JOIN Examinations e ON sub.subject_name = e.subject_name AND s.student_id = e.student_id
+GROUP BY subject_name, student_id
+ORDER BY student_id, subject_name
